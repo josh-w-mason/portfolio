e76608f4a83e536ee570ede1c268f2064afe46dc
Josh Mason <josh.mason.music@gmail.com>
2023-05-01 12:30:26 +1200

    style nav logos, still WIP

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,36 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import linkedinLogo from '../icons/png-transparent-linkedin-icon-linkedin-text-rectangle-logo.png'
+import githubLogo from '../icons/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png'
 
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="projects">Projects</Link>
-        </li>
-        <li>
-          <a href="https://github.com/josh-w-mason">
-            <img
-              src={
-                'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
-              }
-              alt="github logo"
-            ></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/joshua-walcott-mason">
-            <img src={linkedinLogo} alt="Linked In logo"></img>
-          </a>
-        </li>
-      </ul>
+      <div className="Nav-logo nav-list">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="projects">Projects</Link>
+          </li>
+
+          <li>
+            <a href="https://github.com/josh-w-mason">
+              <img src={githubLogo} alt="github logo"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/joshua-walcott-mason">
+              <img src={linkedinLogo} alt="Linked In logo"></img>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
